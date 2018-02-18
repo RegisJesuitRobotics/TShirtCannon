9f91e25699dd76c1f50be9ceceeee07635f2172a
2018DriverStation <no@email.com>
2018-02-18 14:34:58 -0700

    Made it without talons for testing purposes

--- src/org/usfirst/frc/team3729/robot/Robot.java
+++ src/org/usfirst/frc/team3729/robot/Robot.java
@@ -31,7 +31,7 @@ public class Robot extends IterativeRobot {
 	TShirtControl tShirtControl;
 	boolean isShooting, isShootEnabled;
 	double channel6Value;
-	
+
 	@Override
 	public void robotInit() {
 
@@ -51,9 +51,9 @@ public class Robot extends IterativeRobot {
 	}
 
 	/**
-	 * This function is called once each time the robot enters Disabled mode.
-	 * You can use it to reset any subsystem information you want to clear when
-	 * the robot is disabled.
+	 * This function is called once each time the robot enters Disabled mode. You
+	 * can use it to reset any subsystem information you want to clear when the
+	 * robot is disabled.
 	 */
 	@Override
 	public void disabledInit() {
@@ -67,14 +67,14 @@ public class Robot extends IterativeRobot {
 
 	/**
 	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString code to get the auto name from the text box below the Gyro
+	 * between different autonomous modes using the dashboard. The sendable chooser
+	 * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+	 * remove all of the chooser code and uncomment the getString code to get the
+	 * auto name from the text box below the Gyro
 	 *
 	 * You can add additional auto modes by adding additional commands to the
-	 * chooser code above (like the commented example) or additional comparisons
-	 * to the switch structure below with additional strings & commands.
+	 * chooser code above (like the commented example) or additional comparisons to
+	 * the switch structure below with additional strings & commands.
 	 */
 	@Override
 	public void autonomousInit() {
@@ -102,9 +102,9 @@ public class Robot extends IterativeRobot {
 		}
 
 		tShirtControl.TShirtDrive();
-		tShirtControl.CannonMovement();
+		// tShirtControl.CannonMovement();
 		tShirtControl.charging();
-		
+
 		// SHOOT TIMER
 
 		if (playStation.ButtonL1() && tShirtControl.isTankCharged()) {
